d311729ec6f665182fd234ad45ca480c4f91784f
Michaela Rif <160975275+MichaelaRif@users.noreply.github.com>
2024-12-02 23:05:21 +0200

    Renamed table and columns to follow naming conventions

--- app/src/main/java/com/example/codingquizzes/quizzes/data/local/QuizListDao.kt
+++ app/src/main/java/com/example/codingquizzes/quizzes/data/local/QuizListDao.kt
@@ -10,16 +10,16 @@ import com.example.codingquizzes.quizzes.data.model.Quiz
 
 @Dao
 interface QuizListDao {
-    @Query("SELECT * FROM quiz_list_table")
+    @Query("SELECT * FROM quizzes")
     fun getQuizzes(): LiveData<List<Quiz>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(quiz: Quiz)
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertAll(quizzes: List<Quiz>)
+    suspend fun insertAll(quizlist: List<Quiz>)
 
-    @Query("UPDATE quiz_list_table SET title= :title, description= :description, prerequisite= :prerequisite WHERE id= :id")
+    @Query("UPDATE quizzes SET quiz_title= :title, quiz_description= :description, quiz_prerequisite= :prerequisite WHERE id= :id")
     suspend fun update(id: Int, title: String, description: String, prerequisite: String)
 
     @Delete
--- app/src/main/java/com/example/codingquizzes/quizzes/data/model/Quiz.kt
+++ app/src/main/java/com/example/codingquizzes/quizzes/data/model/Quiz.kt
@@ -4,10 +4,10 @@
     import androidx.room.Entity
     import androidx.room.PrimaryKey
 
-    @Entity(tableName = "quiz_list_table")
+    @Entity(tableName = "quizzes")
     data class Quiz(
         @PrimaryKey(autoGenerate = true) var id: Int,
-        @ColumnInfo(name = "title") val title: String,
-        @ColumnInfo(name = "description") val description: String,
-        @ColumnInfo(name = "prerequisite") val prerequisite: String
+        @ColumnInfo(name = "quiz_title") val title: String,
+        @ColumnInfo(name = "quiz_description") val description: String,
+        @ColumnInfo(name = "quiz_prerequisite") val prerequisite: String
     )
